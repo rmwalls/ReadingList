9fc954381ee8472d68b8c132a656202dfdd0dca8
ZelleM <groovyrlm@gmail.com>
2019-09-08 20:42:14 -0400

    Some consolidation

--- server.js
+++ server.js
@@ -19,15 +19,13 @@ app.use(routes);
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/googlebooks", { useNewUrlParser: true });
 
-
-
 // Define API routes here
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+//app.get("*", (req, res) => {
+//  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+//});
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
